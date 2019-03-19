ff4fa207288d539fd89f09da9fe2384f8b579225
Ralph Schoon <rsjazz777@gmail.com>
2019-03-19 13:50:34 +0100

    Update wcl.bat

--- com.ibm.js.team.workitem.commandline/scripts/wcl.bat
+++ com.ibm.js.team.workitem.commandline/scripts/wcl.bat
@@ -1,4 +1,4 @@
 rem set JAVA_HOME=C:\PROGRA~1\Java\jre1.8.0_191
 set PLAIN_JAVA=C:\RTC606Dev\Installs\PlainJavaAPI
 
-%JAVA_HOME%\bin\java -Djava.security.policy=rmi_no.policy -Djava.ext.dirs=./lib;%PLAIN_JAVA%;%JAVA_HOME%/jre/lib/ext -cp ./lib;%PLAIN_JAVA% -jar wcl.jar %*
+"%JAVA_HOME%\bin\java" -Djava.security.policy=rmi_no.policy -Djava.ext.dirs="./lib;%PLAIN_JAVA%;%JAVA_HOME%/jre/lib/ext" -cp "./lib;%PLAIN_JAVA%" -jar wcl.jar%*
