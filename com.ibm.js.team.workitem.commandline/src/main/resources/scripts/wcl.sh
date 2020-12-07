f717e6d1f2ba721e9040f0dc8e0c9ecadbf66bae
Ralph Schoon <ralph.schoon@de.ibm.com>
2020-12-07 14:10:28 +0100

    Fix unix shell script

--- com.ibm.js.team.workitem.commandline/src/main/resources/scripts/wcl.sh
+++ com.ibm.js.team.workitem.commandline/src/main/resources/scripts/wcl.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#JAVA_HOME=/usr/java/jdk1.8.0_20/bin
-#export JAVA_HOME
-
-PLAIN_JAVA=/usr/RTC606Dev/Installs/PlainJavaAPI
+JAVA_HOME=/usr/local/openjdk-8
+export JAVA_HOME
+PLAIN_JAVA=/var/plainjava
 export PLAIN_JAVA
 
 # In the -Djava.ext.dirs always keep the Java distribution in the front
-$JAVA_HOME/bin/java -Djava.security.policy=rmi_no.policy -Djava.ext.dirs=$JAVA_HOME/lib/ext:$JAVA_HOME/jre/lib/ext:./lib:$PLAIN_JAVA -cp ./lib:$PLAIN_JAVA% -jar wcl.jar "$@"
+$JAVA_HOME/bin/java -Djava.security.policy=rmi_no.policy -Djava.ext.dirs=$JAVA_HOME/lib/ext:$JAVA_HOME/jre/lib/ext:./lib:$PLAIN_JAVA -cp ./lib:$PLAIN_JAVA -jar WCL.jar "$@"
