bd8247e878f393a42fc070083443355cf1a07e7c
Ralph Schoon <ralph.schoon@de.ibm.com>
2021-02-10 08:24:49 +0100

    Update IWorkItemCommandLineConstants.java

--- com.ibm.js.team.workitem.commandline/src/main/java/com/ibm/js/team/workitem/commandline/IWorkItemCommandLineConstants.java
+++ com.ibm.js.team.workitem.commandline/src/main/java/com/ibm/js/team/workitem/commandline/IWorkItemCommandLineConstants.java
@@ -13,7 +13,7 @@ package com.ibm.js.team.workitem.commandline;
  */
 public interface IWorkItemCommandLineConstants {
 
-	public static final String VERSIONINFO = "5.3";
+	public static final String VERSIONINFO = "5.3.1";
  
 	// Commands available
 	public static final String COMMAND_CREATE = "create";
